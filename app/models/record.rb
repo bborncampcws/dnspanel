bbfd314a107336e44f9a6ac2180574ba886bb8f5
Brian Borncamp <bborncamp@cws.net>
2012-10-24 12:51:06 -0400

    fix validation for records

--- app/models/record.rb
+++ app/models/record.rb
@@ -1,6 +1,6 @@
 class TargetValidator <  ActiveModel::EachValidator
   def validate_each(record,attribute,value)
-    unless record.recordType=~/A/ and (value=~/[A-z]/).nil?
+    if !(record.recordType=~/IN.A/).nil? and !(value=~/[A-z]/).nil?
       record.errors[:name]<<'A Record must be IP address'
     end
   end
@@ -9,7 +9,7 @@ end
 class Record < ActiveRecord::Base
   belongs_to :zone
   attr_accessible :source, :target, :recordType
-  validates :recordType, :inclusion => {:in => ['IN	MX 0','IN	MX 10','IN	MX 20', 'IN	A', 'IN	CNAME','IN MX 0','IN MX 10','IN MX 20', 'IN A', 'IN CNAME'], :message=> "%{value} is not an allowable record type"}
+  validates :recordType, :inclusion => {:in => ['IN MX 0','IN MX 10','IN MX 20', 'IN A', 'IN CNAME'], :message=> "%{value} is not an allowable record type"}
   validates :target, :presence=>true, :target=>true
   validates :source, :presence=>true
 end
